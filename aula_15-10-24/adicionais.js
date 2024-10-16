6dfbc03190aaf4dfed3eaf4816b7ab8b56a35740
Yasmim <yascneves@hotmail.com>
2024-10-15 21:34:11 -0300

    introducao ao js

---
+++ aula_15-10-24/adicionais.js
@@ -0,0 +1,4 @@
+function entrarEmContato(){
+    var whatsapp = prompt("Digite seu telefone:");
+    document.write("Entraremos em contato com você atravésdo número ",+whatsapp);
+}
---
+++ aula_15-10-24/script.js
@@ -0,0 +1,20 @@
+var nome = "Yasmim";
+
+if(nome == "Yasmim"){
+    var saida = "O nome digitado foi Yasmim"
+}
+
+alert(saida);
+
+const saudacao = "Diga oi!";
+
+var idade = prompt("Digite sua idade:");
+alert(idade);
+console.log(typeof idade);
+
+document.write("<h1>É um prazer recebê-lo aqui! Volte sempre</h1>");
+document.write("<br>");
+document.write("<h1>Ficamos felizes por esse dia!</h1>");
+document.write("<img src= 'https://cff2.earth.com/uploads/2019/03/07150511/jasmineflower.jpg'");
+
+
